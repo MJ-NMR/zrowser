33e22c7ec2184e33801552c75c912f8f7eaff456
Zaater <almogtbanmr123@gmail.com>
2025-05-05 21:03:05 +0300

    add local file protocol

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const net = require('node:net');
 const tls = require('tls');
+const fs = require('fs');
 
 class browser {
 	constructor(url) {
 		[this.schema, url] = url.split('://');
-		[this.host, this.path] = url.split('/', 1);
-		this.path = (this.path) ? '/' + this.path : '/';
+		let hostAndPath = url.split('/');
+		this.host = hostAndPath.shift(); // First item is host:port
+		this.path = '/' + hostAndPath.join('/'); // Recombine rest as path
 		[this.host, this.port] = this.host.split(':');
 		this.headers = {};
 		this.body = null;
@@ -22,12 +24,27 @@ class browser {
 				servername: this.host,
 				rejectUnauthorized: true
 			});
-		} else {		
+		} else if(this.schema === 'file') {
+			fs.stat(this.path, (err, stats) => {
+				if (err) throw err;
+				if (stats.isFile()) {
+					fs.readFile(this.path, (err, data) => {
+						if (err) throw err;
+						this.content = data.toString();
+						this.show(true);
+					});
+				} else {
+					console.error('Path is not a file:', this.path);
+				}
+			});
+			return;
+		} else {
 			clint = net.connect({host: this.host, port: port}); 
 		}
 		let respond = '';
 		let request = `GET ${this.path} HTTP/1.1\r\n`;
 		request += `Host: ${this.host}\r\n`;
+		request += `User-Agent: Zrowser\r\n`;
 		request += `Connection: close\r\n`;  // <- force server to close
 		request += "\r\n";
 		clint.setEncoding('utf8');
@@ -59,10 +76,10 @@ class browser {
 		return st; 
 	}
 
-	show() {
+	show(file) {
 		let tag = false;
 		let char;
-		if(!this.body) throw new Error("there are no body");
+		if(!this.body && !file) throw new Error("there are no body");
 		if(!this.content) {
 			for(let i = 0;i < this.body.length; i++) {
 				char = this.body[i];
