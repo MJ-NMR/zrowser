91c8a8b8838d328e8986babf8da61c8359fd67b2
Zaater <almogtbanmr123@gmail.com>
2025-05-08 17:25:25 +0300

    format the files

--- app.js
+++ app.js
@@ -1,101 +1,46 @@
-const net = require('node:net');
-const tls = require('tls');
-const fs = require('fs');
+const { request, Url, handleResponse } = require('./request.js');
 
 class browser {
-	constructor(url) {
-		[this.schema, url] = url.split('://');
-		let hostAndPath = url.split('/');
-		this.host = hostAndPath.shift(); // First item is host:port
-		this.path = '/' + hostAndPath.join('/'); // Recombine rest as path
-		[this.host, this.port] = this.host.split(':');
-		this.headers = {};
-		this.body = null;
-		this.content = '';
+	constructor(){
+		this.cash = {};
+		this.cookies = {};
+	} 
+
+	async run(urlStr) {
+		const url = new Url(urlStr);
+		if(this.cash[url.fullUrl]) this.show(this.cash[url.fullUrl][2])
+		else {
+			let response = await request(url);
+			if(response[0] === 'html') {
+				response[1] = handleResponse(response[1]);
+			}
+			this.cash[url.fullUrl] = response;
+			this.show(response);
+		}	
 	}
 
-	request() {
-		let clint;
-		const port = this.port || (this.schema === 'https' ? 443 : 80);
-		if(this.schema === 'https') {
-			clint = tls.connect({
-				host: this.host,
-				port: port,
-				servername: this.host,
-				rejectUnauthorized: true
-			});
-		} else if(this.schema === 'file') {
-			fs.stat(this.path, (err, stats) => {
-				if (err) throw err;
-				if (stats.isFile()) {
-					fs.readFile(this.path, (err, data) => {
-						if (err) throw err;
-						this.content = data.toString();
-						this.show(true);
-					});
-				} else {
-					console.error('Path is not a file:', this.path);
-				}
-			});
-			return;
-		} else {
-			clint = net.connect({host: this.host, port: port}); 
-		}
-		let respond = '';
-		let request = `GET ${this.path} HTTP/1.1\r\n`;
-		request += `Host: ${this.host}\r\n`;
-		request += `User-Agent: Zrowser\r\n`;
-		request += `Connection: close\r\n`;  // <- force server to close
-		request += "\r\n";
-		clint.setEncoding('utf8');
-		clint.on(this.schema === 'https' ? 'secureConnect' : 'connect', () => {
-			console.log('connected');
-			clint.write(request);
-		});
-		clint.on('data', (data) => {
-			respond += data;
-		});
-		clint.on('end', () => {
-			this.handleResponse(respond);
-			this.show();
-		});
-		clint.on('error', (error) => {
-			console.log(error);
-		});
-	}
-
-	handleResponse(res) {
-		let header, body;
-		[ header, body] = res.split('\r\n\r\n');
-		this.body = body;
-		header = header.split('\r\n');
-		const st = header.shift();
-		header.forEach((line) => {
-			this.headers[line.split(':')[0]] = line.split(': ')[1];
-		});
-		return st; 
-	}
-
-	show(file) {
+	show(res) {
 		let tag = false;
 		let char;
-		if(!this.body && !file) throw new Error("there are no body");
-		if(!this.content) {
-			for(let i = 0;i < this.body.length; i++) {
+		let data = res[1];
+		if(!data) throw new Error("there are no body or data");
+		if(res[0] !== 'text') {
+			data = '';
+			for(let i = 0; i < data.length; i++) {
 				char = this.body[i];
 				if(char === '<') tag = true;
-				if(!tag) this.content += char;
+				if(!tag) data += char;
 				if(char === '>') tag = false;
 			}
 		}
-		console.log(this.content);
+		console.log(data);
 	}
 }
 
+const local = new browser();
 if(process.argv[2]) {
-	const url = process.argv[2];
-	const local = new browser(url);
-	local.request();
+	const urlStr = process.argv[2];
+	local.run(urlStr);
 } else {
 	console.log('zrowser URl \n  OR \nnode app.js URL');
 };
---
+++ request.js
@@ -0,0 +1,90 @@
+const net = require('net');
+const tls = require('tls');
+const fs = require('fs');
+
+class Url {
+	constructor(urlStr) {
+		this.fullUrl = urlStr;
+		let rest;
+		[this.schema, rest] = urlStr.split('://');
+		let hostAndPath = rest.split('/');
+		this.host = hostAndPath.shift(); // First item is host:port
+		this.path = '/' + hostAndPath.join('/'); // Recombine rest as path
+		let port;
+		[this.host, port] = this.host.split(':');
+		this.port = port || (this.schema === 'https' ? 443 : 80);
+	}
+}
+const request = async (url) => {
+	console.log(url);
+	let clint;
+	const port = url.port; 
+	let respond = '';
+
+	if(url.schema === 'file') {
+		const stats = await fs.promises.stat(url.path);
+		if (stats.isFile()) {
+			respond = await fs.promises.readFile(url.path, { encoding : "utf8"});
+			return [ 'text', respond ]
+		} else {
+			return console.log('Path is not a file:', url.path);
+		}
+	}
+
+	if(url.schema === 'https') {
+		clint = await new Promise((resolve) => {
+			socket =  tls.connect({
+				host: url.host,
+				port: port,
+				servername: url.host,
+				rejectUnauthorized: true
+			}, () => {
+				console.log('connected');
+				resolve(socket);
+			});
+		});
+
+	} else if(url.schema === 'http') {
+		clint = await new Promise((resolve) => {
+			const socket =	net.connect({host: url.host, port: port}, () => {
+				console.log('connected');
+				resolve(socket);
+			});
+		});
+	}
+	const data = await new Promise((resolve, reject) => {
+		let respond = '';
+		let request = `GET ${url.path} HTTP/1.1\r\n`;
+		request += `Host: ${url.host}\r\n`;
+		request += `User-Agent: Zrowser\r\n`;
+		request += `Connection: close\r\n`;  // <- force server to close
+		request += "\r\n";
+		clint.setEncoding('utf8');
+		clint.write(request);
+		clint.on('data', (data) => {
+			console.log('Received chunk:', data);
+			respond += data;
+		});
+		clint.on('end', () => {
+			resolve( [ 'html', respond ] );
+		});
+		clint.on('error', (error) => {
+			reject(error);
+		});
+	});
+	return data;
+};
+
+const handleResponse = (res) => {
+	let header, body;
+	[ header, body] = res.split('\r\n\r\n');
+	header = header.split('\r\n');
+	const st = header.shift();
+	const headers = {};
+	header.forEach((line) => {
+		headers[line.split(':')[0]] = line.split(': ')[1];
+	});
+	return [ [ st, headers ], body];
+}
+
+module.exports = { Url, request, handleResponse };
